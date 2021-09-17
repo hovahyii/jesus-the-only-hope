b25a28a494891a46dd9c7de80180c7e9a5fc2ab5
Jehovah Yii Zui Hon (Hovah) <31881261+hovahyii@users.noreply.github.com>
2021-09-17 10:45:36 +0800

    moving db to postgresql

---
+++ prisma/migrations/20210917022116_prayer_request/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Prayer" (
+    "id" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Prayer_pkey" PRIMARY KEY ("id")
+);
